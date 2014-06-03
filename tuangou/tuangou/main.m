48520944f0891413164d87eeff09e696cec38fc4
xiongzenghui <ios>
2014-06-03 17:42:13 +0800

    试试怎么用
    没有什么东西，一个空工程

---
+++ tuangou/tuangou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tuangou
+//
+//  Created by xiongzenghui on 14-6-3.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
